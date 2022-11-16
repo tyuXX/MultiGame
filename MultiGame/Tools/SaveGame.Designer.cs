2aaab173eaa251f42d5253f6e0398fdebf54415f
GokalpSen <gokalpsen09@gmail.com>
2022-11-17 00:03:38 +0300

    push17

--- MultiGame/Tools/SaveGame.Designer.cs
+++ MultiGame/Tools/SaveGame.Designer.cs
@@ -31,6 +31,7 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.savefiledia = new System.Windows.Forms.SaveFileDialog();
             this.SuspendLayout();
             // 
             // textBox1
@@ -48,6 +49,7 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "...";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -57,6 +59,7 @@
             this.button2.TabIndex = 2;
             this.button2.Text = "Save Game";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // SaveGame
             // 
@@ -68,6 +71,7 @@
             this.Controls.Add(this.textBox1);
             this.Name = "SaveGame";
             this.Text = "SaveGame";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.SaveGame_FormClosing);
             this.Load += new System.EventHandler(this.SaveGame_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -79,5 +83,6 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.SaveFileDialog savefiledia;
     }
 }
--- MultiGame/Tools/SaveGame.cs
+++ MultiGame/Tools/SaveGame.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static MultiGame.rntm;
 
 namespace MultiGame.Tools
 {
@@ -19,7 +21,61 @@ namespace MultiGame.Tools
 
         private void SaveGame_Load(object sender, EventArgs e)
         {
+            log.Add("[" + DateTime.Now + "][From:Internal/Tool/SaveGame/Form]{(Log)}Opening Form");
+        }
 
+        private void SaveGame_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            log.Add("[" + DateTime.Now + "][From:Internal/Tool/SaveGame/Form]{(Log)}Closing Form");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            savefiledia.Title = "Save Game File Of MultiGame";
+            if (savefiledia.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = savefiledia.FileName;
+                log.Add("[" + DateTime.Now + "][From:Internal/Tool/SaveGame/FileSelector]{(Log)}Selected File Name:" + '"' + savefiledia.FileName + '"'); ;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox1.Text))
+            {
+                string[] masterfile =
+                {
+                    encode(logupdateinterval.ToString()),
+                    encode(autoclickerinterval.ToString()),
+                    encode(money.ToString()),
+                    encode(generation.ToString()),
+                    encode(autogenmult.ToString()),
+                    encode(level.ToString()),
+                    encode(xp.ToString()),
+                    encode(xpn.ToString()),
+                    encode(add1.ToString()),
+                    encode(add2.ToString()),
+                    encode(add3.ToString()),
+                    encode(add4.ToString()),
+                    encode(add5.ToString()),
+                    encode(add6.ToString()),
+                    encode(add7.ToString()),
+                    encode(add8.ToString()),
+                    encode(add9.ToString()),
+                    encode(add10.ToString()),
+                    encode(mult1.ToString()),
+                    encode(mult2.ToString()),
+                    encode(mult3.ToString()),
+                    encode(mult4.ToString()),
+                    encode(mult5.ToString()),
+                    encode(mult6.ToString()),
+                    encode(mult7.ToString()),
+                    encode(mult8.ToString()),
+                    encode(mult9.ToString()),
+                    encode(mult10.ToString())
+                };
+                File.WriteAllLines(textBox1.Text,masterfile);
+            }
         }
     }
 }
